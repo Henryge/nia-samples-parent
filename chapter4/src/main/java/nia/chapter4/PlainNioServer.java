ec1490fb5aa7eb5b2b01bb6aa05787bb3d1dfdc7
Henryge <gefei@gefeideMacBook-Pro.local>
2018-12-06 22:05:13 +0800

    first commit

---
+++ chapter4/src/main/java/nia/chapter4/PlainNioServer.java
@@ -0,0 +1,63 @@
+package nia.chapter4;
+
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+public class PlainNioServer {
+	public void serve(int port) throws Exception {
+		ServerSocketChannel serverChannel = ServerSocketChannel.open();
+		serverChannel.configureBlocking(false);
+		ServerSocket ss = serverChannel.socket();
+		InetSocketAddress address = new InetSocketAddress(port);
+		ss.bind(address);
+		Selector selector = Selector.open();
+		serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+		final ByteBuffer msg = ByteBuffer.wrap("Hi!\r\n".getBytes());
+		for(;;) {
+			try {
+				selector.select();
+			} catch (Exception e) {
+				e.printStackTrace();
+				break;
+			}
+			Set<SelectionKey> readyKeys = selector.selectedKeys();
+			Iterator<SelectionKey> iterator = readyKeys.iterator();
+			while(iterator.hasNext()) {
+				SelectionKey key = iterator.next();
+				iterator.remove();
+				try {
+					if(key.isAcceptable()) {
+						ServerSocketChannel server = (ServerSocketChannel) key.channel();
+						SocketChannel client = server.accept();
+						client.configureBlocking(false);
+						client.register(selector, SelectionKey.OP_WRITE | SelectionKey.OP_READ, msg.duplicate());
+						System.out.println("Accepted connection from " + client);
+					}
+					if(key.isWritable()) {
+						SocketChannel client = (SocketChannel) key.channel();
+						ByteBuffer buffer = (ByteBuffer) key.attachment();
+						while(buffer.hasRemaining()) {
+							if(client.write(buffer) == 0) {
+								break;
+							}
+						}
+						client.close();
+					}
+				} catch (Exception e) {
+					key.cancel();
+					try {
+						key.channel().close();
+					} catch (Exception e1) {
+					}
+				}
+			}
+		}
+	}
+}
